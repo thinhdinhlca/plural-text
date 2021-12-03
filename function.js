086c80e93b55c8ce81c8c3bb085b6e483e32f847
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-12-03 20:06:37 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+
+window.function = function (txt, number) {
+
+  txt = txt.value ?? "";
+  number = number.value ?? 0;
+  let index = txt.indexOf("/");
+  let len = txt.length;
+  number = Math.round(number);
+
+  if (index == -1 || number == "")
+    txt = "0 " + txt.substring(index+1,len);
+
+  else if (number > 1 || number == 0)
+    txt = number + " " + txt.substring(index+1,len);
+
+  else
+    txt = number + " " + txt.substring(0,index);
+
+  return txt;
+}
